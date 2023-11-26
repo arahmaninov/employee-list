766b4000acfd0378e1ab627ec556f48ec4941b0e
arahmaninov <arahmaninov@yandex.ru>
2023-11-26 19:11:51 +0300

    add employee pages

--- app.py
+++ app.py
@@ -6,3 +6,29 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/employees")
+def show_employees():
+    con = sqlite3.connect("database.db")
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM employee")
+
+    rows = cur.fetchall()
+    con.close()
+
+    return render_template("employees.html", rows=rows)
+
+@app.route("/newemployee")
+def new_employee():
+    con = sqlite3.connect("database.db")
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM position ORDER BY title")
+
+    rows = cur.fetchall()
+    con.close()
+
+    return render_template("newemployee.html", rows=rows)
