6ef2cd3e92f394a84349c710722a84fec935bdcf
arahmaninov <arahmaninov@yandex.ru>
2023-12-03 15:03:39 +0300

    add forms

--- employeelist/employees/views.py
+++ employeelist/employees/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Employee
+from .forms import EmployeeForm
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
 def employee_list(request):
-    return render(request, 'employee_list.html', {'employees': Employee.objects.all()} ) 
+    return render(request, 'employee_list.html', {
+        'employees': Employee.objects.all(),
+        'form': EmployeeForm(),
+        }) 
