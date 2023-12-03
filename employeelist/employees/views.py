41007a8eaa4e43ecab0b75588b89a264f0332a24
arahmaninov <arahmaninov@yandex.ru>
2023-12-03 15:58:38 +0300

    add create employee

--- employeelist/employees/views.py
+++ employeelist/employees/views.py
@@ -7,7 +7,15 @@ def index(request):
     return render(request, "index.html")
 
 def employee_list(request):
-    return render(request, 'employee_list.html', {
-        'employees': Employee.objects.all(),
-        'form': EmployeeForm(),
-        }) 
+    context = {}
+    employees = Employee.objects.all()
+    form = EmployeeForm()
+    context['employees'] = employees
+    context['form'] = form
+
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            form = EmployeeForm(request.POST)
+            form.save()
+
+    return render(request, 'employee_list.html', context) 
